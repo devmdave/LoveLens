da70c78dd26bd20ecbc32fc29ed818077e1f02c6
devmdave <davemadhav2007@gmail.com>
2025-11-01 23:55:37 +0530

    changed api key and improved the prompt

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({apiKey: 'AIzaSyBsP7J1SdZCeEayvvlRn1DBcuAOQC6QWU0'})],
   model: 'googleai/gemini-2.5-flash',
 });
